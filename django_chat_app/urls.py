649e6f0bd6620783839d9549f152a3b698b67b8c
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-06-18 09:24:35 +0300

    Delete django_chat_app/urls.py

--- django_chat_app/urls.py
+++
@@ -1,9 +0,0 @@
-# chat_project/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('chat.urls')),
-]
